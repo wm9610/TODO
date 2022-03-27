ea3a58bc2f706eb152c78bc370c5a1b60f708f66
wmcheng <cwm96107@gmail.com>
2022-03-27 23:40:14 +0800

    Remove CORS module

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');
@@ -10,9 +10,9 @@ connectToDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(cors());
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(cors());
+// }
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
