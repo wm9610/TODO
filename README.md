42144b4c2881466aee58b34f186b979f5fc329ed
wmcheng <cwm96107@gmail.com>
2022-03-27 22:55:31 +0800

    prepare to deploy

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 
 This project is to consolidate the learning in MERN stack web development.
 
-This project provides functionality to create new TODO items, and track the existing TODO items. 
+This project provides functionality to create new TODO items, and track the existing TODO items.
 It support new user registration and display the different list of TODO items based on user.
 
 The frontend part is built with React and Material UI, while backend part is built with Express and connected to MongoDB.
 
-Improvement to this project in future: 
-- jwt and password encryption implementation
+Improvement to this project in future:
+
+- jwt and password encryption implementation //done
 - error handling for frontend and backend
 - customize theme and dark mode for Material UI
 - edit TODO item
-- use cloud MongoDB instead of local
+- use cloud MongoDB instead of local //done
 - deploy website
--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');
 const todoRoutes = require('./routes/todoRoutes');
@@ -9,13 +10,27 @@ connectToDB();
 
 const app = express();
 
-app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors());
+}
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/users', userRoutes);
 app.use('/api/todos', todoRoutes);
+
+//serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set environment mode'));
+}
 
 app.use((err, req, res, next) => {
   res.status(500);
