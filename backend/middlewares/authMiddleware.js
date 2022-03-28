0784fdf1c4545a22f173661efe78015f604bbabb
wmcheng <cwm96107@gmail.com>
2022-03-28 21:32:54 +0800

    Fix server crash when first login

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) => {
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
       if (err) {
-        res.status(403).json('Token is not valid!');
+        return res.status(403).json('Token is not valid!');
       }
       //   req.user = user;
       next();
